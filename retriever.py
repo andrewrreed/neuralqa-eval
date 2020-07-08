db914c2f7d9e068c1e3296b2c3de146427819fd0
areed1242 <areed1242#gmail.com>
2020-07-08 09:14:11 -0400

    Create QueryExpander

--- retriever.py
+++ retriever.py
@@ -140,3 +140,153 @@ class Retriever:
         question_text = qe.expanded_question if hasattr(qe, 'expanded_question') else question_text
         
         return results, question_text
+
+
+class QueryExpander:
+    '''
+    Query expansion utility class that augments an ElasticSearch query with optional techniques
+    including Named Entity Recognition and Synonym Expansion
+    
+    Args:
+        question_text
+        entity_args (dict) - Ex. {'spacy_model': nlp}
+        synonym_args (dict) - Ex. {'gensim_model': word_vectors, 'n_syns': 3}
+    
+    '''
+    
+    def __init__(self, question_text, entity_args=None, synonym_args=None):
+        
+        self.question_text = question_text
+        self.entity_args = entity_args
+        self.synonym_args = synonym_args
+
+        if self.synonym_args and not self.entity_args:
+            raise Exception('Cannot do synonym expansion without NER! Expanding synonyms on named entities reduces recall.')
+
+        if self.synonym_args or self.entity_args:
+            self.nlp = self.entity_args['spacy_model']
+            self.doc = self.nlp(self.question_text)
+        
+        self.build_query()
+        
+    def build_query(self):
+
+        # build entity sub-query
+        if self.entity_args:
+            self.extract_entities()
+        
+        # identify terms to expand
+        if self.synonym_args:
+            self.identify_terms_to_expand()
+        
+        # build question sub-query
+        self.construct_question_query()
+        
+        # combine sub-queries
+        sub_queries = []
+        sub_queries.append(self.question_sub_query)
+        if hasattr(self, 'entity_sub_queries'):
+            sub_queries.extend(self.entity_sub_queries)
+            
+        query = Q('bool', should=[*sub_queries])
+        self.query = query
+        
+    
+    def extract_entities(self):
+        '''
+        Extracts named entities using spaCy and constructs phrase match sub-queries
+        for each. Saves both entities and sub-queries as attributes.
+        
+        '''
+        
+        entity_list = [entity.text.lower() for entity in self.doc.ents]
+        
+        entity_sub_queries = []
+        
+        for ent in entity_list:
+            eq = Q('multi_match',
+                   query=ent,
+                   type='phrase',
+                   fields=['title', 'text'])
+            
+            entity_sub_queries.append(eq)
+        
+        self.entities = entity_list
+        self.entity_sub_queries = entity_sub_queries
+        
+        
+    def identify_terms_to_expand(self):
+        '''
+        
+        '''
+        if hasattr(self, 'entities'):
+            # get unique list of entity tokens
+            entity_terms = [ent.split(' ') for ent in self.entities]
+            entity_terms = [ent for sublist in entity_terms for ent in sublist]
+        else:
+            entity_terms = []
+    
+        # terms to expand not part of entity or a stopword
+        terms_to_expand = [term for term in self.doc if \
+                           (term.lower_ not in entity_terms) and (not term.is_stop)\
+                            and (not term.is_digit) and (not term.is_punct) and 
+                            (not len(term.lower_)==1 and term.is_alpha)]
+        
+        self.terms_to_expand = terms_to_expand
+
+        
+    def construct_question_query(self):
+        '''
+        Builds a multi-match query from the raw question text extended with synonyms 
+        for eligible any terms (i.e. terms that are not part of an entity or stopword)
+
+        '''
+        
+        if hasattr(self, 'terms_to_expand'):
+            
+            syns = []
+            for term in self.terms_to_expand:
+                syns.extend(self.gather_synonyms_static(term, self.synonym_args['n_syns']))
+            
+            question = self.question_text + ' ' + ' '.join(syns)
+            self.expanded_question = question
+            self.all_syns = syns
+        
+        else:
+            question = self.question_text
+        
+        qq = Q('multi_match',
+               query=question,
+               type='most_fields',
+               fields=['title', 'text'])
+        
+        self.question_sub_query = qq
+        
+
+    def gather_synonyms_static(self, token, n_syns):
+        '''
+        Takes in a token and returns a specified number of synonyms defined by
+        cosine similarity of word vectors. Uses stemming to ensure none of the
+        returned synonymns share the same stem (ex. photo and photos cant happen)
+        
+        '''
+        try:
+            syns = self.synonym_args['gensim_model'].similar_by_word(token.lower_)
+
+            lemmas = []
+            final_terms = []
+            for item in syns:
+                term = item[0]
+                lemma = self.nlp(term)[0].lemma_
+
+                if lemma in lemmas:
+                    continue
+                else:
+                    lemmas.append(lemma)
+                    final_terms.append(term)
+                    if len(final_terms) == n_syns:
+                        break
+        except:
+            final_terms = []
+
+        return final_terms
